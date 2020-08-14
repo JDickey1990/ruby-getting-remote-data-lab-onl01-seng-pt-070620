38dd74469fd6dc6ef720f27164d8273b3d9eb7c1
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-14 02:09:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,10 +6,13 @@ class GetRequester
   
   
   def initialize(url)
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
+    @URL=url
   end 
 
   def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
 end 
